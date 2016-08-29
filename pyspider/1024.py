2c754d651d236781123de703a2a91e77083615fc
Fan Yu <yfann2012@163.com>
2016-08-29 21:54:41 +0800

    finish 1024

--- pyspider/1024.py
+++ pyspider/1024.py
@@ -4,8 +4,9 @@
 # Project: cao
 
 from pyspider.libs.base_handler import *
+from pymongo import MongoClient
 
-
+        
 class Handler(BaseHandler):
     crawl_config = {
     }
@@ -13,6 +14,7 @@ class Handler(BaseHandler):
         self.baseUrl='http://aimeizi.ml/thread0806.php?fid=16&search=&page='
         self.page_num=1
         self.total_num=1
+        self.store=MongoStore('mongodb://localhost:27017/')
 
     @every(minutes=24 * 60)
     def on_start(self):
@@ -23,6 +25,25 @@ class Handler(BaseHandler):
             self.page_num+=1
 
     @config(age=10 * 24 * 60 * 60)
-    def index_page(self, response):
-        for each in response.doc('.tr3.t_one').items():
-            print each('h3 a').attr.href
+    def index_page(self, response):    
+        results=[{"title":each('h3 a').text(),
+         "url":each('h3 a').attr.href,
+         "author":each('td:nth-child(3) a').text(),
+         "authorUrl":each('td:nth-child(3) a').attr.href} for each in response.doc('.tr3.t_one').items()]
+        self.store.insert_all(results)
+        
+
+        
+class MongoStore(object):
+    
+    def __init__(self,connectionStr):
+        self.client=MongoClient(connectionStr)
+        self.db=self.client['caoDB']
+        self.collection=self.db['dgrCol']
+        
+    def insert(self,obj):
+        return self.collection.insert_one(obj).inserted_id
+    
+    def insert_all(self,objs):
+        result=self.collection.insert_many(objs)
+        return result.inserted_ids
--- tools/mongo.py
+++ tools/mongo.py
@@ -2,14 +2,13 @@ import datetime
 from pymongo import MongoClient
 
 class MongoStore(object):
-
     def __init__(self,connectionStr):
         self.client=MongoClient(connectionStr)
         self.db=self.client['blog']
         self.collection=self.db['articles']
     
     def insert(self,obj):
-         return self.collection.insert_one(obj).inserted_id
+        return self.collection.insert_one(obj).inserted_id
 
     def insert_all(self,objs):
         result=self.collection.insert_many(objs)
